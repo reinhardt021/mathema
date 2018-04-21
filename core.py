e5d6cafe0b07b7f441f64199cb41ea08efb07817
Reinhardt Cagara <reinhardt.cgr@gmail.com>
2018-04-21 10:26:14 -0700

    have basic interaction

--- core.py
+++ core.py
@@ -1,1 +1,14 @@
-print "Hi there"
+def greetings():
+    print "Hi :) I'm Mathema"
+
+
+greetings()
+
+response = raw_input("You must be ... ")
+
+print "Pleasure to meet you " + response
+
+
+
+#  is there a way to just run this on??
+# maybe a script somewhere?
